556ba474d533df41e814e01f4b74e2d2054459d8
crea <masataka0504@gmail.com>
2017-04-18 13:21:57 -0700

    Initial Commit

---
+++ MyFirstObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MyFirstObjC
+//
+//  Created by mas.t on 2017/04/18.
+//  Copyright © 2017年 masa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
